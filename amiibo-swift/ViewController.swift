999461325aea7939b3b18b0d0d2269ad2bf152cf
surachet.y <aottapkub@hotmail.com>
2021-05-24 16:02:50 +0700

    Initial Commit

---
+++ amiibo-swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  amiibo-swift
+//
+//  Created by Surachat Yaitammasan on 24/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
